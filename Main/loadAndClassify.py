ee58250b1593977f81017387cecee89d80acbf9b
JonasRosenzweig <Jdrosenzweig@gmail.com>
2021-08-10 16:37:15 +0200

    Third commit - added if statements to loadAndClassify.py

--- Main/loadAndClassify.py
+++ Main/loadAndClassify.py
@@ -1,82 +1,80 @@
 import os
 import pandas as pd
-import numpy as np
 import pickle
-import seaborn as sns
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
-from sklearn.metrics import roc_curve, roc_auc_score, precision_recall_curve, f1_score, auc
-from keras.models import Sequential
-from keras.layers import LSTM, Activation, Dense, Dropout, Input, Embedding
-from keras.preprocessing.text import Tokenizer
 from keras.preprocessing import sequence
-from keras.callbacks import EarlyStopping
-import itertools
-import matplotlib.pyplot as plt
-import random
-import tensorflow as tf
+import keras
+import re
 
-model = keras.models.load_model('../input/notebook5267d82d06/RNN1.h5')
+SAVEPATH = r'C:\Users\surface\Desktop\YouWe\MLDM\Data\Classified Data'
+CSV = r'C:\Users\surface\Desktop\YouWe\MLDM\Data\2D Data\vaiva.csv'
+DATE_TIME_REGEX = '([0-9]|0[0-9]|1[0-9])-([0-9][0-9]|[0-9])-[0-9]{4} ([0-9]|0[0-9]|1[0-9])(.)[0-9]{2}:[0-9]{2}'
+
+
+
+model = keras.models.load_model(r'C:\Users\surface\Desktop\YouWe\MLDM\Main\Models\4ClassSimple.h5')
 print("Model Loaded")
-tokenizer = open('../input/notebook5267d82d06/tokenizer.pkl', 'rb')
+tokenizer = open(r'C:\Users\surface\Desktop\YouWe\MLDM\Main\Models\tokenizer.pkl', 'rb')
 print("Tokenizer loaded")
 tok = pickle.load(tokenizer)
-df = pd.read_csv("../input/clean2d14k/CleanData2D.csv", names=["text", "target"],
-                 encoding="ISO-8859-1", skiprows=1, low_memory=False, index_col=False)
-print("Data Loaded")
-df=df.sample(frac=1)
-print("Data Shuffled")
-df=df.astype(str)
-print("Data type-cast to String")
-df1k=df.head(1000)
-print("Df sliced over 1k rows")
 le = LabelEncoder()
 print("LabelEncoder Loaded")
-targets = df.target
-targets = le.fit_transform(targets)
-print("LabelEncoder Fitted")
-df1k.head(10)
+
 
 def predictClass(text):
     text_pad = sequence.pad_sequences(tok.texts_to_sequences([text]), maxlen=300)
     score = le.inverse_transform(model.predict_classes([text_pad]))
     return score[0]
 
-def dfPredicter(df):
-    pred = []
-    for i in df['text']:
-        score = predictClass(i)
-        pred.append(score)
-        print(i, score)
 
-    Table = {'input': df['text'],
-             'target': df['target'],
-             'pred': pred}
-    dfPred = pd.DataFrame(Table)
-    dfPred.to_csv('testPredict2.csv')
+def append_print(a, i, s):
+    a.append(s)
+    print(i, s)
+
 
 def csvPredicter(csv):
     df = pd.read_csv(csv, names=['input', 'target'], encoding="ISO-8859-1", skiprows=1,
                      low_memory=False, index_col=False)
+    df.astype(str)
+    targets = df.target
+    targets = le.fit_transform(targets)
+    print("LabelEncoder Fitted")
     pred = []
-    for i in df['input']:
-        score = predictClass(i)
-        pred.append(score)
-        print(i, score)
 
-    Table = {'input': df['input'],
+    for i in df['input']:
+        if i == 'True' or i == 'False':
+            score = 'BOOL'
+            append_print(pred, i, score)
+        elif i == 'DKK' or i == 'SEK' or i == 'EUR':
+            score = 'CURRENCY_CODE'
+            append_print(pred, i, score)
+        elif i == '26':
+            score = 'LANGUAGE_ID'
+            append_print(pred, i, score)
+        elif '.jpg' in i or '.jpeg' in i:
+            score = 'PROD_PHOTO_URL'
+            append_print(pred, i, score)
+        elif '.html' in i or 'https://' in i:
+            score = 'DIRECT_LINK'
+            append_print(pred, i, score)
+        elif i.isnumeric():
+            score = 'NUMERIC_ID/AMT'
+            append_print(pred, i, score)
+        elif re.search(DATE_TIME_REGEX, i):
+            score = 'DATE_TIME'
+            append_print(pred, i, score)
+        else:
+            score = 'UNKNOWN'
+            # score = predictClass(i)
+            append_print(pred, i, score)
+    table = {'input': df['input'],
              'target': df['target'],
              'pred': pred}
-    dfPred = pd.DataFrame(Table)
-    dfPred.to_csv('testPredict2.csv')
-
-# dfPredicter(df1k)
-
-# csvPredicter("../input/clean2d14k/CleanData2D.csv")
-
-targets = dfPred
+    dfPred = pd.DataFrame(table)
+    os.chdir(SAVEPATH)
+    dfPred.to_csv('testPredict.csv')
 
 
+csvPredicter(CSV)
 
+# (([123]0|[012][1-9]|31).(0[1-9]|1[012]).(19[0-9]{2}|2[0-9]{3}) ){0,1}([01][0-9]|2[0-3])(.(([0-5][0-9]))){0,5}
---
+++ Main/regex_test.py
@@ -0,0 +1,19 @@
+import re
+
+pycharm_format = '03-05-2021 13:07:56'
+excel_format = '03/05/2021 13.07.56'
+format3 = '03/05/2021 13:07:56'
+
+
+DATE_TIME_REGEX = '([0-9]|0[0-9]|1[0-9])-([0-9][0-9]|[0-9])-[0-9]{4} ([0-9]|0[0-9]|1[0-9])(.)[0-9]{2}:[0-9]{2}'
+
+match = re.search(DATE_TIME_REGEX, pycharm_format)
+match2 = re.search(DATE_TIME_REGEX, excel_format)
+match3 = re.search(DATE_TIME_REGEX, format3)
+
+boolean = bool(match)
+boolean2 = bool(match2)
+boolean3 = bool(match3)
+
+print(boolean, boolean2, boolean3)
+

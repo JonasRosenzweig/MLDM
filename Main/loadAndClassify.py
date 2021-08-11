33bf0a72ce9ce370ca0ebafdcd16176ef21a4dcb
JonasRosenzweig <Jdrosenzweig@gmail.com>
2021-08-11 14:45:15 +0200

    7th commit, added compare to % unknowns

--- Main/loadAndClassify.py
+++ Main/loadAndClassify.py
@@ -7,7 +7,7 @@ import keras
 import re
 
 SAVEPATH = r'C:\Users\surface\Desktop\YouWe\MLDM\Data\Classified Data'
-CSV = r'C:\Users\surface\Desktop\YouWe\MLDM\Data\2D Data\milkywalk.csv'
+CSV = r'C:\Users\surface\Desktop\YouWe\MLDM\Data\2D Data\vaiva.csv'
 
 DATE_TIME_REGEX = '([0-9]|0[0-9]|1[0-9])-([0-9][0-9]|[0-9])-[0-9]{4} ([0-9]|0[0-9]|1[0-9])(.)[0-9]{2}:[0-9]{2}$'
 PRICE_REGEX = '^(\d{1,5})$|^(\d{1,5},\d{1,2})$|^(\d{1,2}\.\d{3,3})$|^(\d{1,2}\.\d{3,3},\d{1,2})$'
@@ -78,12 +78,12 @@ def csvPredicter(csv):
              'pred': pred}
     dfPred = pd.DataFrame(table)
     os.chdir(SAVEPATH)
-    dfPred.to_csv('testPredict_milkywalk.csv')
+    dfPred.to_csv('testPredict_vaiva.csv')
 
 
 csvPredicter(CSV)
 
-test_CSV = r'C:\Users\surface\Desktop\YouWe\MLDM\Data\Classified Data\testPredict_milkywalk.csv'
+test_CSV = r'C:\Users\surface\Desktop\YouWe\MLDM\Data\Classified Data\testPredict_vaiva.csv'
 comp_df = df = pd.read_csv(test_CSV, names=['input', 'target', 'pred'], encoding="ISO-8859-1", skiprows=1,
                            low_memory=False, index_col=0)
 compared = comp_df['target'] == comp_df['pred']
@@ -93,29 +93,32 @@ table = {'input': df['input'],
          'compared': compared}
 comp_df = pd.DataFrame(table)
 os.chdir(SAVEPATH)
-comp_df.to_csv('testPredictCompared_milkywalk.csv')
+comp_df.to_csv('testPredictCompared_vaiva.csv')
 
 count_T = comp_df.compared.sum()
 count_F = (len(comp_df) - (comp_df.compared.sum()))
 count_B = (comp_df.pred == 'BOOL').sum()
+count_U = (comp_df.pred == 'UNKNOWN').sum()
 
 print(count_T, 'Correct Classifications out of', len(comp_df))
 print(count_F, 'Incorrect Classifications out of', len(comp_df))
 accuracy = (count_T/len(comp_df))*100
 bool_percent = (count_B/len(comp_df))*100
+unknown_percent = (count_U/len(comp_df))*100
 print('Accuracy:', "{:.2f}".format(accuracy), '%')
 print('Boolean:', "{:.2f}".format(bool_percent), '%')
+print('Unknown:', "{:.2f}".format(unknown_percent), '%')
 print('Semi-Classified Accuracy with Booleans:', '{:.2f}'.format(accuracy+bool_percent), '%')
+print('Semi-Classified Accuracy (total minus unknowns):', '{:.2f}'.format(100-unknown_percent), '%')
 
-
-# not classified with if statements (12):
+# not classified with if statements (12): -- UNKNOWN
 # PROD_NUM, PROD_NAME, PROD_SORT, PROD_SITE_SPECIFIC_SORTING, VENDOR_NUM, PROD_UNIT_ID, PROD_DELIVERY
 # PROD_CREATED_BY, PROD_EDITED_BY, DESC_SHORT, DESC_LONG, DESC_LONG_2
 
-# if statements/REGEX classified exact (5):
+# if statements/REGEX classified exact (5): -- EXACT
 # LANGUAGE_ID, PROD_WEIGHT, PROD_PHOTO_URL, CURRENCY_CODE, DIRECT_LINK
 
-# if statements/REGEX semi-classified (21):
+# if statements/REGEX semi-classified (21): -- SEMI-CLASSIFIED
 # NUMERIC_ID/AMT (9): PROD_MIN_BUY, PROD_MAX_BUY, PROD_TYPE_ID, INTERNAL_ID, STOCK_COUNT, STOCK_LIMIT, PROD_VIEWED
 # NUMERIC_ID/AMT: PROD_SALES_COUNT, PROD_CAT_ID
 # PRICE (3): PROD_RETAIl_PRICE, PROD_COST,  UNIT_PRICE
@@ -124,5 +127,5 @@ print('Semi-Classified Accuracy with Booleans:', '{:.2f}'.format(accuracy+bool_p
 # DATE_TIME (2): PROD_CREATED, PROD_EDITED
 
 # 5th commit, only regex no models: 13.65% accuracy on vaiva.csv, 10.82% on milkywalk.csv
-# 6th commit, checking with booleans, still no model: 35.49% on vaiva.csv (21.84% bool), 28.15% on milkywalk (17.33% bool)
-
+# 6th commit, checking with booleans: 35.49% on vaiva.csv (21.84% bool), 28.15% on milkywalk (17.33% bool)
+# 7th commit: checking % unknowns: 73.83% vaiva (35.49% unknown), 64.62% milywalk (35.38% unknown)

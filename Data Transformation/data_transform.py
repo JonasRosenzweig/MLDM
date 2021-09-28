51e9aa0c7e545816582b2209c615bdd9f590ab50
Jonas Rosenzweig <Jdrosenzweig@gmail.com>
2021-09-28 11:09:43 +0200

    data transform MVP 1

--- Data Transformation/data_transform.py
+++ Data Transformation/data_transform.py
@@ -13,7 +13,7 @@ df_list = []
 i = 0
 
 for j in range(len(files)):
-    print(i + len(files),  'of', (len(files)), "files remaining.")
+    print(i + len(files), 'of', (len(files)), "files remaining.")
     dataset_filename = os.listdir(DATAPATH)[j]
     dataset_path = os.path.join("..", DATAPATH, dataset_filename)
     data = pd.read_csv(dataset_path)
@@ -30,47 +30,55 @@ unused_headers = ['PROD_PDF_URL_TEXT_3', 'PROD_PDF_URL_TEXT_2', 'PROD_PDF_URL_TE
                   'PROD_FILE_URL', 'FIELD_8', 'PROD_NOTES', 'PROD_FRONT_PAGE_PERIOD_ID', 'FIELD_6',
                   'PROD_PDF_URL_2', 'PROD_PDF_URL', 'FIELD_3', 'PROD_UNIQUE_URL_NAME', 'FIELD_2', 'PROD_NEW_PERIOD_ID',
                   'FIELD_5', 'PROD_DELIVER_NOT_IN_STOCK', 'PROD_PICTURE_ALT_TEXT', 'PROD_GOOGLE_FEED_CATEGORY',
-                  'PROD_LOCATION_NUMBER', 'DESC_LONG_2', 'DESC_SHORT', 'PROD_SEARCHWORD', 'VENDOR_NUM', 'INTERNAL_ID',
-                  'PROD_TYPE_ID', 'MANUFAC_ID', 'PROD_CAT_ID']
-removed_headers = ['PROD_HIDDEN', 'PROD_VAR_MASTER', 'PROD_NEW', 'PROD_FRONT_PAGE', 'TOPLIST_HIDDEN', 'OMIT_FROM_FREE_SHIPPING_LIMIT',
-                   'PROD_CREATED', 'PROD_EDITED', 'PROD_RETAIl_PRICE', 'PROD_COST',  'UNIT_PRICE', 'PROD_COST_PRICE',
+                  'PROD_LOCATION_NUMBER', 'DESC_LONG_2', 'DESC_SHORT', 'PROD_SEARCHWORD', 'PROD_CREATED_BY', 'PROD_EDITED_BY']
+removed_headers = ['PROD_HIDDEN', 'PROD_VAR_MASTER', 'PROD_NEW', 'PROD_FRONT_PAGE', 'TOPLIST_HIDDEN',
+                   'OMIT_FROM_FREE_SHIPPING_LIMIT',
+                   'PROD_CREATED', 'PROD_EDITED',
                    'LANGUAGE_ID', 'PROD_WEIGHT', 'PROD_PHOTO_URL', 'CURRENCY_CODE', 'DIRECT_LINK',
-                   'PROD_SHOW_ON_GOOGLE_FEED', 'PROD_BARCODE_NUMBER', 'PROD_SHOW_ON_FACEBOOK_FEED', 'PROD_UNIQUE_URL_NAME',
-                   'PROD_SHOW_ON_PRICERUNNER_FEED', 'PROD_SHOW_ON_KELKOO_FEED', 'PROD_RETAIL_PRICE', 'PROD_PDF_URL', 'PROD_PDF_URL_2',
-                   'PROD_PDF_URL_3', 'PROD_DELIVERY', 'PROD_DELIVERY_NOT_IN_STOCK', 'FIELD_1', 'FIELD_2', 'FIELD_3', 'FIELD_4', 'FIELD_5',
-                   'FIELD_6', 'FIELD_7', 'FIELD_8', 'FIELD_9', 'FIELD_10', 'PROD_FILE_URL', 'ïï\x06Óµ\x07L¸Úb.j"ÿ"%\x165´', 'EDBPriser_NUM',
+                   'PROD_SHOW_ON_GOOGLE_FEED', 'PROD_BARCODE_NUMBER', 'PROD_SHOW_ON_FACEBOOK_FEED',
+                   'PROD_UNIQUE_URL_NAME',
+                   'PROD_SHOW_ON_PRICERUNNER_FEED', 'PROD_SHOW_ON_KELKOO_FEED', 'PROD_RETAIL_PRICE', 'PROD_PDF_URL',
+                   'PROD_PDF_URL_2',
+                   'PROD_PDF_URL_3', 'PROD_DELIVERY', 'PROD_DELIVERY_NOT_IN_STOCK', 'FIELD_1', 'FIELD_2', 'FIELD_3',
+                   'FIELD_4', 'FIELD_5',
+                   'FIELD_6', 'FIELD_7', 'FIELD_8', 'FIELD_9', 'FIELD_10', 'PROD_FILE_URL',
+                   'ïï\x06Óµ\x07L¸Úb.j"ÿ"%\x165´', 'EDBPriser_NUM',
                    'PROD_SORT', 'PROD_SITE_SPECIFIC_SORTING', 'PROD_UNIT_ID', 'PROD_NUM']
-kept_headers = ['PROD_NAME', 'TITLE', 'PROD_CREATED_BY', 'PROD_EDITED_BY', 'DESC_LONG', 'META_DESCRIPTION', 'MANUFAC_ID',
-                'PROD_CAT_ID']
-count = ['PROD_SALES_COUNT', 'STOCK_COUNT', 'STOCK_LIMIT', 'PROD_VIEWED', 'PROD_MIN_BUY', 'PROD_MAX_BUY', 'PROD_MIN_BUY_B2B']
-description = ['DESC_LONG', 'META_DESCRIPTION']
+kept_headers = ['PROD_NAME', 'TITLE', 'DESC_LONG', 'META_DESCRIPTION', 'VENDOR_NUM', 'INTERNAL_ID', 'PROD_TYPE_ID',
+                'MANUFAC_ID', 'PROD_CAT_ID', 'PROD_RETAIl_PRICE', 'PROD_COST', 'UNIT_PRICE', 'PROD_COST_PRICE']
+count = ['PROD_SALES_COUNT', 'STOCK_COUNT', 'STOCK_LIMIT', 'PROD_VIEWED', 'PROD_MIN_BUY', 'PROD_MAX_BUY',
+         'PROD_MIN_BUY_B2B']
+price = ['PROD_RETAIl_PRICE', 'PROD_COST', 'UNIT_PRICE', 'PROD_COST_PRICE',]
+description = ['DESC_LONG', 'META_DESCRIPTION', 'DESC_LONG_2']
+ID = ['VENDOR_NUM', 'INTERNAL_ID', 'PROD_TYPE_ID', 'MANUFAC_ID', 'PROD_CAT_ID']
+title = ['PROD_NAME', 'TITLE']
 author = ['PROD_CREATED_BY', 'PROD_EDITED_BY']
-#kept_headers = []
+# kept_headers = []
 # BOOL: PROD_SHOW_ON_GOOGLE_FEED]
 print('Nunique headers:', df.Header.nunique())
 print('unique headers:', df.Header.unique())
 
 df = df[df.Header.str.contains('|'.join(kept_headers))]
-df = df[~df.Header.str.contains('DESC_LONG_2')]
-#df = df[~df.Header.str.contains('|'.join(count))]
-#df = df[~df.Header.str.contains('|'.join(description))]
-#df = df[~df.Header.str.contains('|'.join(removed_headers))]
-#df = df[~df.Header.str.contains('|'.join(unused_headers))]
+#df = df[~df.Header.str.contains('DESC_LONG_2')]
+# df = df[~df.Header.str.contains('|'.join(count))]
+# df = df[~df.Header.str.contains('|'.join(description))]
+# df = df[~df.Header.str.contains('|'.join(removed_headers))]
+# df = df[~df.Header.str.contains('|'.join(unused_headers))]
 df = df.sample(frac=1)
-for n in range(len(author)):
-    df = df.replace(author[n], 'AUTHOR')
-# for i in range(len(removed_headers)):
-#     df = df.replace(removed_headers[i], 'UNKNOWN')
-# for j in range(len(unused_headers)):
-#     df = df.replace(unused_headers[j], 'UNKNOWN')
-# for k in range(len(count)):
-#     df = df.replace(count[k], 'COUNT')
+for n in range(len(price)):
+    df = df.replace(price[n], 'PRICE')
+for i in range(len(description)):
+    df = df.replace(description[i], 'DESCRIPTION')
+for j in range(len(ID)):
+    df = df.replace(ID[j], 'ID')
+for k in range(len(title)):
+    df = df.replace(title[k], 'PROD_TITLE')
 df = df.head(100)
 print('Removed unwanted headers.')
-df.to_csv('7_class_PNAME_AUT_TIT_DESCL_DESCM_MANID_PCATID_100.csv', index=False)
+df.to_csv('MVP_train_PRICE_DESC_ID_TIT_100.csv', index=False)
 print('Done saving transformed file.')
 print('Nunique headers:', df.Header.nunique())
 print('unique headers:', df.Header.unique())
- #['DESC_LONG' 'MANUFAC_ID' 'PROD_NAME' 'PROD_NUM' 'TITLE' 'META_DESCRIPTION'] # -> last one
+# ['DESC_LONG' 'MANUFAC_ID' 'PROD_NAME' 'PROD_NUM' 'TITLE' 'META_DESCRIPTION'] # -> last one
 
- #['MANUFAC_ID', 'INTERNAL_ID', 'COUNT', 'PROD_TYPE_ID', 'COUNT'] # -> new one
+# ['MANUFAC_ID', 'INTERNAL_ID', 'COUNT', 'PROD_TYPE_ID', 'COUNT'] # -> new one
